2a5ebd68bcce3374714323ea1c098c98d1d984fd
mireajava <pecherni@gmail.com>
2025-08-24 13:01:29 +0300

    Create Converter.java

---
+++ src/main/java/edu/course/Converter.java
@@ -0,0 +1,14 @@
+package edu.course;
+
+public class Converter {
+    private static final double ROUBLES_PER_YUAN = 11.91;
+
+    public static double toRubles(int yuan) {
+        // TODO: студент должен реализовать
+        return 0.0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Программа-конвертер. Реализуйте метод toRubles().");
+    }
+}
